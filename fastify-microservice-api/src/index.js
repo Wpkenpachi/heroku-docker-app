99ef9fe2c54e697b9068786ee7c6c0422148d99f
Wesley Paulo <angelportinari17@gmail.com>
2021-03-12 12:47:27 -0300

    Changed environments variables

--- fastify-microservice-api/src/index.js
+++ fastify-microservice-api/src/index.js
@@ -2,7 +2,7 @@ require('make-promises-safe')
 require('dotenv').config()
 const fastify = require('fastify')({ logger: true })
 
-const { FAST_API_HOST: HOST, PORT } = process.env;
+const { FAST_API_HOST: HOST, PORT, FAST_API_PORT } = process.env;
 
 fastify.register(require('fastify-postgres'), {
   connectionString: process.env.CONNECTION_STRING,
@@ -14,7 +14,7 @@ fastify.register(require('fastify-postgres'), {
 fastify.register(require('./routes'))
 fastify.register(require('./routes/products'))
 
-fastify.listen(PORT, HOST, function (err, address) {
+fastify.listen(PORT || FAST_API_PORT, HOST, function (err, address) {
   if (err) {
     fastify.log.error(err)
     process.exit(1)
